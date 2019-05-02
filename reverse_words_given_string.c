912b3aa26d58a96b7846055079697c9f1b18ddf1
Naveen M V <30305957+naveencode@users.noreply.github.com>
2019-05-02 14:07:05 +0530

    Create reverse_words_given_string.c

---
+++ reverse_words_given_string.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+char s[2000];
+char a[2000][2000];
+
+int joinStrings(char a[2000][2000], char s[]) {
+  int i=0, j=0, k=0;
+  int len = strlen(s);
+  while (j<len && s[j] != '\0') {
+    i = 0;
+    while (j<len && s[j] != '.') {
+      a[k][i] = s[j];
+      i++;
+      j++;
+    }
+    a[k][i] = '\0';
+    k++;
+    j++;
+  }
+  return k;
+}
+
+int main() {
+  freopen("reverse_words.txt", "r", stdin);
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    scanf("%s", s);
+    int n = joinStrings(a, s);
+    for (int i=n-1; i>=0; i--) {
+      printf("%s.", a[i]);
+    }
+
+  }
+}
